743949ce9617da9aaf1b1e13cdfee87cc1e91d5f
Roman Usyk <romanysik@gmail.com>
2019-09-23 12:57:04 +0300

    Reindent

--- main/main.py
+++ main/main.py
@@ -3,34 +3,34 @@ from main.db_utils import db_connect, select, insert, update, delete
 
 
 def main():
-	conn = None
-	try:
-		conn = db_connect(db_conf)
-		while True:
-			s = input("Enter command:")
-			if s == "exit":
-				return
-			elif s.startswith("select"):
-				print(select(conn))
-			elif s.startswith("insert"):
-				_, name, password = s.split()
-				print(insert(conn, name, password))
-			elif s.startswith("update"):
-				_, id, field_name, field_value = s.split()
-				print(update(conn, id, field_name, field_value))
-			elif s.startswith("delete"):
-				_, id = s.split()
-				print(delete(conn, id))
-			else:
-				print("Command should be one of:")
-				print("select")
-				print("insert <name> <password>")
-				print("update <id> <field_name> <field_value>")
-				print("delete <id>")
-				print("exit")
-	finally:
-		conn.close()
+    conn = None
+    try:
+        conn = db_connect(db_conf)
+        while True:
+            s = input("Enter command:")
+            if s == "exit":
+                return
+            elif s.startswith("select"):
+                print(select(conn))
+            elif s.startswith("insert"):
+                _, name, password = s.split()
+                print(insert(conn, name, password))
+            elif s.startswith("update"):
+                _, id, field_name, field_value = s.split()
+                print(update(conn, id, field_name, field_value))
+            elif s.startswith("delete"):
+                _, id = s.split()
+                print(delete(conn, id))
+            else:
+                print("Command should be one of:")
+                print("select")
+                print("insert <name> <password>")
+                print("update <id> <field_name> <field_value>")
+                print("delete <id>")
+                print("exit")
+    finally:
+        conn.close()
 
 
 if __name__ == "__main__":
-	main()
+    main()
